b85b9d62053c63f72d7a5d8e07facdef616f7191
Badrinath Ayyamperumal <63338711+Badrinath007@users.noreply.github.com>
2023-09-08 21:58:11 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
  const express = require('express');
  const path = require('path');
+ const fileupload = require('express-fileupload');
 
  //let initial_path = path.join(__dirname, "public");
 
  const app = express();
  app.use(express.static(__dirname));
+ app.use(fileupload());
 
  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "home.html"));
  })
 
  app.get('/editor', (req, res) => {
-   res.sendfile(path.join(__dirname, "editor.html"));
+   res.sendFile(path.join(__dirname, "editor.html"));
+ })
+
+ // upload link
+ app.post('/upload', (req, res) => {
+   let file = req.files.image;
+   let date = new Date();
+   //image name
+   let imagename = date.getDate() + date.getTime() + file.name;
+   //image upload path
+   let path = 'workon/uploads/' + imagename;
+
+   //create upload
+   file.mv(path, (err, result) => {
+      if(err){
+         throw err;
+      } else{
+         //image upload path
+         res.json('uploads/${imagename}')
+      }
+
+   })
  })
 
  app.listen(8080, () => {
